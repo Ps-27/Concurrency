6e373f9d15153a2da792d041d5d5af74e781846e
Prity Sinha <71940538+Ps-27@users.noreply.github.com>
2023-09-16 18:08:15 +0530

    unbuffered channel example

---
+++ channel.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func shout(ping <-chan string, pong chan<- string) {
+	for {
+		s := <-ping
+		pong <- fmt.Sprintf("%s!!!", strings.ToUpper(s))
+	}
+}
+
+func main() {
+
+	//create two channel
+	ping := make(chan string)
+	pong := make(chan string)
+
+	//running in background bcause it's infinite loop
+	go shout(ping, pong)
+
+	// time.Sleep(10 * time.Second)
+
+	fmt.Println("Type of something and press Enter(enter Q to quit)")
+
+	for {
+		//print a prompt
+		fmt.Print("->")
+
+		//get user input
+		var userInput string
+		_, _ = fmt.Scanln(&userInput)
+
+		if userInput == strings.ToLower("q") {
+			break
+		}
+
+		ping <- userInput
+		//wait for a response
+
+		response := <-pong
+		fmt.Println("Response:", response)
+
+	}
+
+	fmt.Println("All done. closing channels.")
+	close(ping)
+	close(pong)
+
+}
+// output:
+// go run channel.go
+// Type of something and press Enter(enter Q to quit)
+// ->prity
+// Response: PRITY!!!
+// ->Pgtyh
+// Response: PGTYH!!!
+// ->q
+// All done. closing channels.
